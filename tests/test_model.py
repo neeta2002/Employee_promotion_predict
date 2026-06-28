a1c68b7156e5049e0c97ab0923a6792fd1374215
Django project <nitadhangar2002@gmail.com>
2026-06-28 20:27:09 +0530

    Added dummy test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,6 @@
 import joblib
-def employee_promotion_prdication():
-    model=joblib.load("model/model.pkl")
-    prediction=model.predict([[8]])
-    assert prediction[0]==1
+
+def test_employee_promotion_prediction():
+    model = joblib.load("model/model.pkl")
+    prediction = model.predict([[8]])
+    assert prediction[0] == 1
